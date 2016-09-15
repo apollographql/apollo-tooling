91077fe200712ba1cbf2aef134c5e0d680aa40a3
Martijn Walraven <martijn@martijnwalraven.com>
2016-09-15 12:14:12 +0200

    Escape string literals in generated code

--- src/swift/codeGenerator.js
+++ src/swift/codeGenerator.js
@@ -59,11 +59,15 @@ function sourceAt(location) {
   return location.source.body.slice(location.start, location.end);
 }
 
+function escapedString(string) {
+  return string.replace(/"/g, '\\"');
+}
+
 function multilineString(string) {
   const lines = string.split('\n');
   return lines.map((line, index) => {
     const isLastLine = index != lines.length - 1;
-    return `"${line}"` + (isLastLine ? ' +' : '');
+    return `"${escapedString(line)}"` + (isLastLine ? ' +' : '');
   }).join('\n');
 }
 
