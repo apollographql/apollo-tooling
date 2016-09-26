36d4e64e700aef8967b949cbe1d4dbbe00d5a3ca
Bobo Diallo <m@bobodiallo.com>
2016-09-26 11:11:23 +0200

    Refactor code generation a bit

--- src/swift/index.js
+++ src/swift/index.js
@@ -1,3 +1,5 @@
+import { camelCase, pascalCase } from 'change-case';
+
 import {
   join,
   block,
@@ -5,17 +7,10 @@ import {
   indent
 } from '../utilities/printing';
 
-import { camelCase, pascalCase } from 'change-case';
-
-import {
-  visit,
-  visitWithTypeInfo,
-  TypeInfo,
-} from 'graphql';
-
-import { typeNameFromGraphQLType, typeDeclarationForGraphQLType } from './types';
-import { propertiesFromFields } from './properties'
 import { escapedString, multilineString } from './strings'
+import { typeNameFromGraphQLType, typeDeclarationForGraphQLType } from './types';
+import { classDeclarationForOperation } from './operations'
+import { classDeclarationForFragment } from './fragments'
 
 export function generateSource(context) {
   const operations = context.compileOperations();
@@ -36,115 +31,4 @@ export function generateSource(context) {
 
 function importDeclarations() {
   return 'import Apollo';
-}
-
-function classDeclarationForOperation({ operationName, variables, fields, source, fragmentsReferenced }) {
-  const className = `${pascalCase(operationName)}Query`;
-  const properties = propertiesFromFields(fields);
-
-  const instancePropertyDeclarations = join(variables.map(variable =>
-    `public let ${variable.name}: ${typeNameFromGraphQLType(variable.type)}`
-  ), '\n');
-
-  const initializerDeclaration = (() => {
-    const initializationParameters = join(variables.map(variable =>
-      `${variable.name}: ${typeNameFromGraphQLType(variable.type)}`
-    ), ', ');
-
-    const propertyInitializations = variables.map(variable =>
-      `self.${variable.name} = ${variable.name}`
-    );
-
-    return `public init(${initializationParameters}) ` + block(propertyInitializations);
-  })();
-
-  const variablesProperty = (() => {
-    if (variables.length < 1) return null;
-
-    const variablesMap = wrap(`return [`, join(variables.map(variable => {
-      return `"${variable.name}": ${variable.name}`;
-    }), ', '), `]`);
-
-    return 'public var variables: GraphQLMap? ' + block([variablesMap])
-  })();
-
-  let queryDocument;
-  if (fragmentsReferenced && fragmentsReferenced.length > 0) {
-    queryDocument = 'public static let queryDocument = ' + join(['operationDefinition', ...fragmentsReferenced.map(fragment =>
-      `.appending(${protocolNameForFragmentName(fragment)}Fragment.fragmentDefinition)`
-    )])
-  }
-
-  return `public final class ${className}: GraphQLQuery ` +
-    block([
-      wrap('', instancePropertyDeclarations, '\n'),
-      initializerDeclaration,
-      wrap('\n', 'public static let operationDefinition =' + indent('\n' + multilineString(source))),
-      wrap('\n', queryDocument),
-      wrap('\n', variablesProperty),
-      wrap('\n', structDeclaration({ name: "Data", properties }))
-    ]);
-}
-
-function structDeclaration({ name, properties = [], fragmentSpreads }) {
-  const propertyDeclarations = properties.map(({ name, typeName }) =>
-    `public let ${name}: ${typeName}`
-  );
-
-  const initializerDeclaration = `public init(map: GraphQLMap) throws ` +
-    block(properties.map(property =>
-      `${property.name} = try map.${ property.isList ? 'list' : 'value' }(forKey: "${property.fieldName}")`));
-
-  const compositeProperties = properties.filter(property => property.isComposite);
-
-  return join([`public struct ${name}: GraphQLMapConvertible`,
-    wrap(', ', join(fragmentSpreads && fragmentSpreads.map(protocolNameForFragmentName), ', ')),
-    ' ',
-    block([
-      wrap('', join(propertyDeclarations, '\n'), '\n'),
-      initializerDeclaration,
-      join(nestedStructDeclarations(compositeProperties), '\n')
-    ])
-  ]);
-}
-
-function nestedStructDeclarations(properties) {
-  let declarations = [];
-  for (const property of properties) {
-    const { unmodifiedTypeName: name, properties, fragmentSpreads, inlineFragments } = property;
-
-    declarations.push(wrap('\n', structDeclaration({ name, properties, fragmentSpreads })));
-
-    for (const inlineFragment of inlineFragments) {
-      const { typeCondition, properties, fragmentSpreads } = inlineFragment;
-
-      declarations.push(wrap('\n',
-        structDeclaration({ name: `${name}_${String(typeCondition)}`, properties, fragmentSpreads })
-      ));
-    }
-  }
-  return declarations;
-}
-
-function protocolNameForFragmentName(fragmentName) {
-  return pascalCase(fragmentName);
-}
-
-function classDeclarationForFragment({ fragmentName, fields, source }) {
-  const protocolName = protocolNameForFragmentName(fragmentName);
-  const className = `${protocolName}Fragment`;
-  const properties = propertiesFromFields(fields);
-
-  return join([
-    `public final class ${className}: GraphQLFragment `,
-    block([
-      'public static let fragmentDefinition =' + indent('\n' + multilineString(source) + '\n'),
-      `public typealias Data = ${protocolName}`
-    ]),
-    '\n\n',
-    `public protocol ${protocolName} `,
-    block(properties.map(({ name, typeName }) =>
-      `var ${name}: ${typeName} { get }`
-    ))
-  ]);
 }
