193c53bce66b622c898cca04f8fd559acf1602a3
Martijn Walraven <martijn@martijnwalraven.com>
2016-12-10 08:36:31 +0100

    Initialize input object with an empty map when no fields are required
    Fixes #22.

--- src/swift/codeGeneration.js
+++ src/swift/codeGeneration.js
@@ -461,7 +461,7 @@ function structDeclarationForInputObjectType(generator, type) {
       generator.withinBlock(() => {
         generator.printOnNewline(wrap(
           `graphQLMap = [`,
-          join(properties.map(({ propertyName }) => `"${propertyName}": ${propertyName}`), ', '),
+          join(properties.map(({ propertyName }) => `"${propertyName}": ${propertyName}`), ', ') || ':',
           `]`
         ));
       });
