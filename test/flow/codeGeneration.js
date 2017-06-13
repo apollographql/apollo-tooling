b9e5505edb88a476365f18da402c879189d2a47b
lewisf <lchung@coursera.org>
2017-06-13 09:05:17 -0700

    fix up tests by passing in addTypename: true

--- test/flow/codeGeneration.js
+++ test/flow/codeGeneration.js
@@ -32,7 +32,7 @@ function setup(schema) {
 
   const compileFromSource = (source) => {
     const document = parse(source);
-    const context = compileToIR(schema, document, { mergeInFieldsFromFragmentSpreads: true } );
+    const context = compileToIR(schema, document, { mergeInFieldsFromFragmentSpreads: true, addTypename: true } );
     generator.context = context;
     return context;
   };
