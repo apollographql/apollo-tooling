385f7edb6684ac93eb3f9ba902db6954f5ec94f1
lewisf <lchung@coursera.org>
2017-06-25 22:04:14 -0700

    Add Typescript/Flow to CLI help + throw error if --tag-name is used on wrong target

--- src/cli.js
+++ src/cli.js
@@ -134,7 +134,7 @@ yargs
       },
       "tag-name": {
         demand: false,
-        describe: "Name of the template literal tag used to identify template literals containing GraphQL queries",
+        describe: "Name of the template literal tag used to identify template literals containing GraphQL queries in Javascript/Typescript code",
         default: 'gql'
       }
     },
@@ -150,6 +150,10 @@ yargs
         .map(input => path.resolve(input))
         // Sort to normalize different glob expansions between different terminals.
         .sort();
+
+      if (argv.tagName && !['flow', 'typescript', 'ts'].includes(argv.target)) {
+        throw new ToolError('The --tag-name flag is only applicable for the "flow" and "typescript" targets. Please specify one of those targets or remove the --tag-name flag')
+      }
 
       const options = {
         passthroughCustomScalars: argv["passthrough-custom-scalars"] || argv["custom-scalars-prefix"] !== '',
