5d53a48cfe9b3ea01f74868113d8a08a24ca69df
Bobo Diallo <m@bobodiallo.com>
2017-07-27 10:22:31 +0200

    Add JSON output tests

---
+++ test/jsonOutput.ts
@@ -0,0 +1,87 @@
+import { parse } from "graphql";
+import { compileToIR } from "../src/compilation";
+import serializeToJSON from "../src/serializeToJSON";
+
+import { loadSchema } from "../src/loading";
+const schema = loadSchema(require.resolve("./starwars/schema.json"));
+
+function compileFromSource(
+  source,
+  options = { mergeInFieldsFromFragmentSpreads: false, addTypename: true }
+) {
+  const document = parse(source);
+  return compileToIR(schema, document, options);
+}
+
+describe("JSON output", function() {
+  test(`should generate JSON output for a query with an enum variable`, function() {
+    const context = compileFromSource(`
+      query HeroName($episode: Episode) {
+        hero(episode: $episode) {
+          name
+        }
+      }
+    `);
+
+    const output = serializeToJSON(context);
+
+    expect(output).toMatchSnapshot();
+  });
+
+  test(`should generate JSON output for a query with a nested selection set`, function() {
+    const context = compileFromSource(`
+      query HeroAndFriendsNames {
+        hero {
+          name
+          friends {
+            name
+          }
+        }
+      }
+    `);
+
+    const output = serializeToJSON(context);
+
+    expect(output).toMatchSnapshot();
+  });
+
+  test(`should generate JSON output for a query with a fragment spread and nested inline fragments`, function() {
+    const context = compileFromSource(`
+      query HeroAndDetails {
+        hero {
+          name
+          ...CharacterDetails
+        }
+      }
+
+      fragment CharacterDetails on Character {
+        name
+        ... on Droid {
+          primaryFunction
+        }
+        ... on Human {
+          height
+        }
+      }
+    `);
+
+    const output = serializeToJSON(context);
+
+    expect(output).toMatchSnapshot();
+  });
+
+  test(`should generate JSON output for a mutation with an enum and an input object variable`, function() {
+    const context = compileFromSource(`
+      mutation CreateReview($episode: Episode, $review: ReviewInput) {
+        createReview(episode: $episode, review: $review) {
+          stars
+          commentary
+        }
+      }
+    `);
+
+    const output = serializeToJSON(context);
+
+    expect(output).toMatchSnapshot();
+  });
+});
