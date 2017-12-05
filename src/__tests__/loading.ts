18fa4c855638215c2b03756c29c7c73a093ee1ac
Lewis Chung <lewis@coursera.org>
2017-12-05 09:25:57 -0800

    Redo filtering out commented out gql strings (#313)
    Redo filtering out commented out gql strings
    
    Instead of using a regex, which is going to make mistakes, pass the documents
    we find through graphql.parse to ensure that it's something we can generate
    types from.

---
+++ src/__tests__/loading.ts
@@ -0,0 +1,70 @@
+import { stripIndents } from 'common-tags';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {
+  extractDocumentFromJavascript,
+  loadAndMergeQueryDocuments
+} from '../loading';
+
+// Test example javascript source files are located within __fixtures__
+describe('extractDocumentFromJavascript', () => {
+  test('normal queries', () => {
+    const contents = fs.readFileSync(path.join(__dirname, '__fixtures__', 'normal.js')).toString();
+    expect(stripIndents`${extractDocumentFromJavascript(contents)}`)
+      .toMatch(
+        stripIndents`
+          query UserProfileView {
+            me {
+              id
+              uuid
+              role
+            }
+          }
+        `
+      );
+  });
+
+  test('comments in template string', () => {
+    const contents = fs.readFileSync(path.join(__dirname, '__fixtures__', 'comments.js')).toString();
+    expect(stripIndents`${extractDocumentFromJavascript(contents)}`)
+      .toMatch(
+        stripIndents`
+          query UserProfileView {
+            me {
+              id
+              # TODO: https://www.fast.com/sdf/sdf
+              uuid
+              # Some other comment
+              role
+            }
+          }
+        `
+      );
+  });
+
+  test('gql completely commented out', () => {
+    const contents = fs.readFileSync(path.join(__dirname, '__fixtures__', 'commentedOut.js')).toString();
+    expect(extractDocumentFromJavascript(contents))
+      .toBeNull();
+  });
+
+  test('invalid gql', () => {
+    const contents = fs.readFileSync(path.join(__dirname, '__fixtures__', 'invalid.js')).toString();
+    expect(extractDocumentFromJavascript(contents))
+      .toBeNull();
+  });
+});
+
+describe('Validation', () => {
+  test(`should extract gql snippet from javascript file`, () => {
+    const inputPaths = [
+      path.join(__dirname, '../../test/fixtures/starwars/gqlQueries.js'),
+    ];
+
+    const document = loadAndMergeQueryDocuments(inputPaths);
+
+    expect(document).toMatchSnapshot();
+  })
+});
+
