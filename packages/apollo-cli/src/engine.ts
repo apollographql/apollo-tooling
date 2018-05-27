adf050345d98ec2b60adef51599d33989bb9be26
James Baxley <james@meteor.com>
2018-05-27 17:56:18 -0400

    clean up typescript for publish

--- packages/apollo-cli/src/engine.ts
+++ packages/apollo-cli/src/engine.ts
@@ -1,8 +1,12 @@
 import { createHttpLink } from "apollo-link-http";
 import fetch from "node-fetch";
+import { ApolloLink } from "apollo-link";
 
-export { ApolloLink } from "apollo-link";
+export { ApolloLink };
 export const getIdFromKey = (key: string) => key.split(":")[1];
 export const ENGINE_URI = "https://schema-registry-current.glitch.me/";
 // XXX looks like node-fetch isn't compatiable typing wise here?
-export const engineLink = createHttpLink({ uri: ENGINE_URI, fetch } as any);
+export const engineLink = createHttpLink({
+  uri: ENGINE_URI,
+  fetch,
+} as any);
