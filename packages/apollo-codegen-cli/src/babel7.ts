bd60ec0f281e5f4dc0cf6e241174872e2ff9e477
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-06-14 11:37:26 -0700

    Don't duplicate Babel typing files

--- packages/apollo-codegen-cli/src/babel7.ts
+++
@@ -1,8 +0,0 @@
-declare module '@babel/types' {
-    export * from 'babel-types';
-}
-declare module '@babel/generator' {
-    export * from 'babel-generator';
-    import Generator from 'babel-generator';
-    export default Generator;
-}
