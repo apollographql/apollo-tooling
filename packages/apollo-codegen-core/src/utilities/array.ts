21105865b6ed3d4a4492337b260401c3d1aa4a92
Shadaj Laddad <shadaj@meteor.com>
2018-06-25 10:57:14 -0700

    Use corejs polyfill for Array.flatMap
    Fixes #439

--- packages/apollo-codegen-core/src/utilities/array.ts
+++ packages/apollo-codegen-core/src/utilities/array.ts
@@ -1,17 +1,8 @@
-export {};
-
 declare global {
   interface Array<T> {
     flatMap<U>(callbackfn: (value: T, index: number, array: T[]) => U[] | undefined, thisArg?: any): U[];
   }
 }
-
-Object.defineProperty(Array.prototype, 'flatMap', {
-  value: function<T,U>(this: Array<T>, callbackfn: (value: T, index: number, array: T[]) => U[], thisArg?: any): U[] {
-    return [].concat.apply([], this.map(callbackfn, thisArg));
-  },
-  enumerable: false
-});
 
 export function maybePush<T = any>(list: T[], item: T) {
   if (!list.includes(item)) {
