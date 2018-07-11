e0eca9e7d3a4ff936a6f772de703676da5864898
Shadaj Laddad <shadaj@meteor.com>
2018-07-11 16:08:17 -0700

    Load settings from an Apollo config file

---
+++ packages/apollo-cli/src/config.ts
@@ -0,0 +1,29 @@
+import { basename, dirname } from "path";
+
+export interface ApolloConfig {
+  schema?: string;
+  endpoint?: string;
+  subscriptionEndpoint?: string;
+  projectName: string;
+  queries: string | string[];
+}
+
+export function loadConfig(obj: any, configFilePath: string): ApolloConfig {
+  if (!obj.schema && !obj.endpoint) {
+    throw new Error("No schema or endpoint specified!");
+  }
+
+  return {
+    schema: (obj.schema || obj.endpoint) as string,
+    endpoint: obj.endpoint,
+    subscriptionEndpoint:
+      obj.subscriptionEndpoint || obj.endpoint.replace("http", "ws"),
+    projectName: basename(dirname(configFilePath)),
+    queries:
+      typeof obj.queries === "string"
+        ? obj.queries
+        : obj.queries
+          ? (obj.queries as string[])
+          : ["**/*.graphql"]
+  };
+}
