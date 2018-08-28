e972c4da9be4136ce55ba6fedf4512d2c29dc4b8
Evans Hauser <evanshauser@gmail.com>
2018-08-28 11:45:23 -0700

    Make query extration import aware
    This approach becomes much too fragile with dynamic imports, so a
    different technique is used in a later commit.

--- packages/apollo-codegen-core/src/loading.ts
+++ packages/apollo-codegen-core/src/loading.ts
@@ -1,5 +1,9 @@
-import { fs } from "./localfs";
+import { fs, withGlobalFS } from "./localfs";
 import { stripIndents } from "common-tags";
+import * as fg from "glob";
+import * as path from "path";
+const astTypes = require("ast-types");
+const recast = require("recast");
 
 import {
   buildClientSchema,
@@ -55,6 +59,161 @@ function filterValidDocuments(documents: string[]) {
   });
 }
 
+export function loadInterpolatedQueries(
+  inputPaths: string[],
+  tagName: string = "gql"
+): string[] {
+  return inputPaths
+    .flatMap(i =>
+      withGlobalFS(() => fg.sync(i, { cwd: process.cwd(), absolute: true }))
+    )
+    .flatMap(inputPath => {
+      if (fs.lstatSync(inputPath).isDirectory()) {
+        return;
+      }
+
+      const body = fs.readFileSync(inputPath, "utf8");
+      if (!body) {
+        return;
+      }
+
+      if (
+        inputPath.endsWith(".jsx") ||
+        inputPath.endsWith(".js") ||
+        inputPath.endsWith(".tsx") ||
+        inputPath.endsWith(".ts")
+      ) {
+        console.log("loading from " + inputPath);
+        return extractAndInterpolateJavascriptDocumets(body.toString(), {
+          tagName,
+          fileRoot: path.basename(inputPath)
+        });
+      }
+
+      if (inputPath.endsWith(".graphql") || inputPath.endsWith(".gql")) {
+        throw new Error("unimplemented");
+      }
+
+      return;
+    })
+    .filter(source => source) as Array<string>;
+}
+
+function extractAndInterpolateJavascriptDocumets(
+  content: string,
+  options: {
+    tagName?: string;
+    fileRoot: string;
+  }
+): string[] {
+  let tagName = options.tagName || "gql";
+
+  const ast = recast.parse(content, {
+    parser: require("recast/parsers/babylon")
+  });
+
+  const mapping = new Map<string, string>();
+  const templates: Array<{
+    expressions: Array<string>;
+    quasis: Array<string>;
+  }> = [];
+  const finished: string[] = [];
+  const variableToValue = new Map<string, string>();
+
+  // isolate the template literals tagged with gql
+  astTypes.visit(ast, {
+    visitTaggedTemplateExpression: (path: any) => {
+      const tag = path.value.tag;
+      // XXX configurable
+      if (tag.name === tagName) {
+        const literal = path.value.quasi;
+
+        // templates without any expressions
+        if ((literal.expressions as Array<any>).length === 0) {
+          finished.push(literal.quasis[0].value.cooked);
+          return false;
+        }
+
+        templates.push({
+          quasis: literal.quasis.map((o: any) => o.value.cooked),
+          expressions: literal.expressions.map((o: any) => o.name)
+        });
+
+        literal.expressions.forEach((exp: any) => {
+          // look for variables from import expressions
+          // XXX need to also include variable declarations -> no dynamic queries
+          const node = path.scope.lookup(exp.name).bindings[exp.name];
+
+          const parent = node[0].parent.value;
+          const parentType = parent.type;
+          switch (parentType) {
+            case "VariableDeclarator":
+              console.log(parent.init.quasi.quasis[0].value.cooked);
+              variableToValue.set(
+                exp.name,
+                parent.init.quasi.quasis[0].value.cooked
+              );
+              break;
+            default:
+              const filePath = node[0].parent.parent.value.source.value;
+              mapping.set(exp.name, filePath);
+          }
+        });
+      }
+      return false;
+    }
+  });
+
+  // scrape for the referenced exports
+  mapping.forEach((file, exportedValue) => {
+    try {
+      [".js", ".jsx", ".ts", ".tsx", ".mjs"].forEach(ext => {
+        const filename =
+          (file.startsWith("/") ? process.cwd() : options.fileRoot || "") +
+          file +
+          ext;
+
+        let contents;
+        if (fs.existsSync(filename)) {
+          contents = fs.readFileSync(filename).toString();
+        } else {
+          return;
+        }
+
+        const ast = recast.parse(contents, {
+          parser: require("recast/parsers/babylon")
+        });
+        astTypes.visit(ast, {
+          visitProgram: (path: any) => {
+            const value = path.scope.getBindings()[exportedValue][0].parent
+              .value.init.quasi.quasis[0].value.cooked;
+
+            variableToValue.set(exportedValue, value);
+            return false;
+          }
+        });
+      });
+      if (!variableToValue.has(exportedValue)) {
+        throw new Error(
+          "Unable to locate import " + file + " with " + options.fileRoot
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  const interpolated = templates.map(({ expressions, quasis }) => {
+    return quasis.reduce((prev, q, i) => {
+      return prev + q + (variableToValue.get(expressions[i]) || "");
+    }, "");
+  });
+
+  finished.push(...interpolated);
+
+  return finished;
+}
+
 export function extractDocumentFromJavascript(
   content: string,
   options: {
@@ -74,6 +233,7 @@ export function extractDocumentFromJavascript(
   }
 
   matches = filterValidDocuments(matches);
+  console.log(matches);
   const doc = matches.join("\n");
   return doc.length ? doc : null;
 }
--- packages/apollo/src/commands/queries/extract.ts
+++ packages/apollo/src/commands/queries/extract.ts
@@ -1,37 +1,37 @@
-import { Command, flags } from '@oclif/command';
-import * as Listr from 'listr';
-import { print } from 'graphql';
-import * as crypto from 'crypto';
-import * as fs from 'fs';
+import { Command, flags } from "@oclif/command";
+import * as Listr from "listr";
+import * as crypto from "crypto";
+import * as fs from "fs";
+import { print } from "graphql";
 
-import { loadQueryDocuments } from 'apollo-codegen-core/lib/loading';
+import { loadQueryDocuments } from "apollo-codegen-core/lib/loading";
 
-import { engineFlags } from '../../engine-cli';
-import { resolveDocumentSets } from '../../config';
-import { loadConfigStep } from '../../load-config';
+import { engineFlags } from "../../engine-cli";
+import { resolveDocumentSets } from "../../config";
+import { loadConfigStep } from "../../load-config";
 
 export default class ExtractQueries extends Command {
-  static description = 'Extracts queries';
+  static description = "Extracts queries";
 
   static flags = {
     help: flags.help({
-      char: 'h',
-      description: 'Show command help',
+      char: "h",
+      description: "Show command help"
     }),
     config: flags.string({
-      description: 'Path to your Apollo config file',
+      description: "Path to your Apollo config file"
     }),
     queries: flags.string({
       description:
-        'Path to your GraphQL queries, can include search tokens like **',
+        "Path to your GraphQL queries, can include search tokens like **"
     }),
     ...engineFlags,
 
     tagName: flags.string({
       description:
-        'Name of the template literal tag used to identify template literals containing GraphQL queries in Javascript/Typescript code',
-      default: 'gql',
-    }),
+        "Name of the template literal tag used to identify template literals containing GraphQL queries in Javascript/Typescript code",
+      default: "gql"
+    })
   };
 
   async run() {
@@ -40,42 +40,50 @@ export default class ExtractQueries extends Command {
     const tasks: Listr = new Listr([
       loadConfigStep(flags, false),
       {
-        title: 'Resolving GraphQL document sets',
+        title: "Resolving GraphQL document sets",
         task: async (ctx, task) => {
           ctx.documentSets = await resolveDocumentSets(ctx.config, false);
           const operations = loadQueryDocuments(
             ctx.documentSets[0].documentPaths,
-            flags.tagName,
+            flags.tagName
           );
           task.title = `Scanning for GraphQL queries (${
             operations.length
           } found)`;
           // XXX send along file information as well
           ctx.operations = operations.map(doc => ({ document: print(doc) }));
-        },
+          // ctx.operations = loadInterpolatedQueries(
+          //   (flags.queries && [flags.queries]) || ctx.documentSets[0].documentPaths,
+          //   flags.tagName
+          // );
+
+          task.title = `Scanning for GraphQL queries (${
+            ctx.operations.length
+          } found)`;
+          // XXX send along file information as well
+        }
       },
       {
-        title: 'Generating hashes',
+        title: "Generating hashes",
         task: async ctx => {
           ctx.mapping = {};
-          (ctx.operations as Array<{ document: string }>).forEach(
-            ({ document }) => {
-              ctx.mapping[
-                crypto
-                  .createHash('sha512')
-                  .update(document)
-                  .digest('hex')
-              ] = document;
-            },
-          );
-        },
+          console.log(ctx.operations);
+          (ctx.operations as Array<string>).forEach(operation => {
+            ctx.mapping[
+              crypto
+                .createHash("sha512")
+                .update(operation)
+                .digest("hex")
+            ] = operation;
+          });
+        }
       },
       {
-        title: 'Outputing manifest',
+        title: "Outputing manifest",
         task: async ctx => {
-          fs.writeFileSync('manifest.json', JSON.stringify(ctx.mapping));
-        },
-      },
+          fs.writeFileSync("manifest.json", JSON.stringify(ctx.mapping));
+        }
+      }
     ]);
 
     return tasks.run();
