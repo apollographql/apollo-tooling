217498e23ce22ef78f6d0cc9ae9881b73cae7f84
James Baxley <james@apollographql.com>
2018-11-12 09:26:58 -0500

    add apollo.config.js and catch operation error

---
+++ packages/apollo-language-server/apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: {
+    name: "Apollo CLI",
+    service: "engine-api-prod"
+  },
+  engine: {
+    frontend: "https://engine-staging.apollographql.com",
+    endpoint: "https://engine-staging-graphql.apollographql.com/api/graphql"
+  }
+};
