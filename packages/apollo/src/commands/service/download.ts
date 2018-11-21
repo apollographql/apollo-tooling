7e9aad1684426eadcf7ba4eb71b12e983a54c00a
Trevor Scheer <trevor@apollographql.com>
2018-11-21 14:13:44 -0800

    Restore skipSSLValidation flag
    This restores the skipSSLValidation flag so a schema introspection
    can be downloaded via http.

--- packages/apollo/src/commands/service/download.ts
+++ packages/apollo/src/commands/service/download.ts
@@ -14,6 +14,10 @@ export default class ServiceDownload extends ProjectCommand {
       char: "t",
       description: "The published tag to check this service against",
       default: "current"
+    }),
+    skipSSLValidation: flags.boolean({
+      char: "k",
+      description: "Allow connections to an SSL site without certs"
     })
   };
 
