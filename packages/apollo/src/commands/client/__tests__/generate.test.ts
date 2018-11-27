038f3188545c46244f2ae275f387a491c8b9dcda
Jake <dawkinsjh@gmail.com>
2018-11-27 12:48:03 -0500

    renamed and cleaned up files

--- packages/apollo/src/commands/client/__tests__/generate.test.ts
+++ packages/apollo/src/commands/client/__tests__/generate.test.ts
@@ -1,90 +1,269 @@
-it("is turned on after summit", () => {});
+// this is because of herkou-cli-utils hacky mocking system on their console logger
+import { stdout, mockConsole } from "heroku-cli-util";
+import * as path from "path";
+import * as fs from "fs";
+import { test as setup } from "apollo-cli-test";
+import {
+  introspectionQuery,
+  print,
+  execute,
+  buildSchema,
+  graphql
+} from "graphql";
+import gql from "graphql-tag";
 
-// jest.mock("apollo-codegen-core/lib/localfs", () => {
-//   return require("../../../../__mocks__/mockFs");
-// });
+const test = setup.do(() => mockConsole());
 
-// // this is because of herkou-cli-utils hacky mocking system on their console logger
-// import { stdout, mockConsole } from "heroku-cli-util";
-// import * as path from "path";
-// import * as fs from "fs";
-// import { test as setup } from "apollo-cli-test";
-// import { introspectionQuery, print, execute, buildSchema } from "graphql";
-// import gql from "graphql-tag";
-// import { fs as mockFS, vol } from "apollo-codegen-core/lib/localfs";
+// helper function to resolve files from the actual filesystem
+const resolveFiles = opts => {
+  let files = {};
+  Object.keys(opts).map(key => {
+    files[key] = fs.readFileSync(path.resolve(__dirname, opts[key]), {
+      encoding: "utf-8"
+    });
+  });
 
-// const test = setup.do(() => mockConsole());
+  return files;
+};
 
-// const graphQLSchema = fs.readFileSync(
-//   path.resolve(__dirname, "../../service/__tests__/fixtures/schema.graphql"),
-//   {
-//     encoding: "utf-8"
-//   }
-// );
+const {
+  graphQLSchema,
+  simpleQuery,
+  otherQuery,
+  clientSideSchema,
+  clientSideSchemaQuery,
+  clientSideOnlySchema,
+  clientSideOnlyQuery
+} = resolveFiles({
+  graphQLSchema: "../../service/__tests__/fixtures/schema.graphql",
+  simpleQuery: "./fixtures/simpleQuery.graphql",
+  otherQuery: "./fixtures/otherQuery.graphql",
+  clientSideSchema: "./fixtures/clientSideSchema.graphql",
+  clientSideSchemaQuery: "./fixtures/clientSideSchemaQuery.graphql",
+  clientSideOnlySchema: "./fixtures/clientSideOnlySchema.graphql",
+  clientSideOnlyQuery: "./fixtures/clientSideOnlyQuery.graphql"
+});
 
-// const fullSchema = execute(buildSchema(graphQLSchema), gql(introspectionQuery))
-//   .data;
+const fullSchema = execute(buildSchema(graphQLSchema), gql(introspectionQuery))
+  .data;
 
-// const simpleQuery = fs.readFileSync(
-//   path.resolve(__dirname, "./fixtures/simpleQuery.graphql")
-// );
+const clientSideSchemaTag = `
+  gql\`
+  ${clientSideSchema}
+  \`
+`;
 
-// const otherQuery = fs.readFileSync(
-//   path.resolve(__dirname, "./fixtures/otherQuery.graphql")
-// );
+const serverSideSchemaTag = `
+  gql\`
+  type Query {
+    hello: String!
+    serverSideField: ServerField!
+  }
 
-// const clientSideSchema = fs.readFileSync(
-//   path.resolve(__dirname, "./fixtures/clientSideSchema.graphql")
-// );
+  type ServerField {
+    serverData: String!
+  }
 
-// const serverSideSchemaTag = `
-// gql\`
-// type Query {
-//   hello: String!
-//   serverSideField: ServerField!
-// }
+  type RemovedField {
+    id: ID!
+    name: RemovedType
+  }
 
-// type ServerField {
-//   serverData: String!
-// }
+  type RemovedType {
+    fieldName: String
+  }
+  \`
+  `;
 
-// type RemovedField {
-//   id: ID!
-//   name: RemovedType
-// }
+describe("client:codegen", () => {
+  test
+    .fs({
+      "./schema.json": JSON.stringify(fullSchema.__schema),
+      "./queryOne.graphql": simpleQuery.toString(),
+      "./apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./**.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.json"
+            }
+          }
+        }
+      `
+    })
+    .command([
+      "client:codegen",
+      "API.swift",
+      "--config=apollo.config.js",
+      "--target=swift"
+    ])
+    .it("writes swift types from local schema", () => {
+      expect(fs.readFileSync("API.swift").toString()).toMatchSnapshot();
+    });
 
-// type RemovedType {
-//   fieldName: String
-// }
-// \`
-// `;
+  test
+    .fs({
+      "schema.graphql": graphQLSchema,
+      "queryOne.graphql": simpleQuery.toString(),
+      "apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./queryOne.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.graphql"
+            }
+          }
+        }
+      `
+    })
+    .command([
+      "client:codegen",
+      "API.swift",
+      "--config=apollo.config.js",
+      "--target=swift"
+    ])
+    .it("writes swift types from local schema in a graphql file", () => {
+      expect(fs.readFileSync("API.swift").toString()).toMatchSnapshot();
+    });
 
-// const clientSideSchemaTag = `
-// gql\`
-// ${clientSideSchema}
-// \`
-// `;
+  test
+    .fs({
+      "schema.json": JSON.stringify(fullSchema.__schema),
+      "queryOne.graphql": simpleQuery.toString(),
+      "apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./queryOne.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.json"
+            }
+          }
+        }
+      `
+    })
+    .command([
+      "client:codegen",
+      "API.swift",
+      "--target=swift",
+      "--operationIdsPath=myOperationIDs.json"
+    ])
+    .it("generates operation IDs for swift files when flag is set", () => {
+      expect(
+        fs.readFileSync("myOperationIDs.json").toString()
+      ).toMatchSnapshot();
+    });
 
-// const clientSideSchemaQuery = fs.readFileSync(
-//   path.resolve(__dirname, "./fixtures/clientSideSchemaQuery.graphql")
-// );
+  // FIXME: doesn't write anything significant to snapshot
+  test
+    .skip()
+    .fs({
+      "schema.json": JSON.stringify(fullSchema.__schema),
+      "queryOne.graphql": simpleQuery.toString(),
+      "queryTwo.graphql": otherQuery.toString(),
+      "apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./**.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.json"
+            }
+          }
+        }
+      `
+    })
+    .command([
+      "client:codegen",
+      "--only=queryTwo.graphql",
+      "--target=swift",
+      "outDirectory"
+    ])
+    .it("handles only flag for Swift target", () => {
+      const [filePath] = fs.readdirSync("./outDirectory");
+      const file = fs.readFileSync(`./outDirectory/${filePath}`).toString();
 
-// const clientSideOnlySchema = fs.readFileSync(
-//   path.resolve(__dirname, "./fixtures/clientSideOnlySchema.graphql")
-// );
+      // expect(file).toMatchSnapshot();
+    });
 
-// const clientSideOnlyQuery = fs.readFileSync(
-//   path.resolve(__dirname, "./fixtures/clientSideOnlyQuery.graphql")
-// );
+  test
+    .fs({
+      "schema.json": JSON.stringify(fullSchema.__schema),
+      "queryOne.graphql": simpleQuery.toString(),
+      "apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./**.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.json"
+            }
+          }
+        }
+      `
+    })
+    .command(["client:codegen", "--target=scala", "API.scala"])
+    .it("writes types for scala", () => {
+      expect(fs.readFileSync("API.scala").toString()).toMatchSnapshot();
+    });
 
-// beforeEach(() => {
-//   vol.reset();
-//   vol.fromJSON({
-//     __blankFileSoDirectoryExists: ""
-//   });
-// });
+  test
+    .fs({
+      "schema.json": JSON.stringify(fullSchema.__schema),
+      "queryOne.graphql": simpleQuery.toString(),
+      "apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./**.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.json"
+            }
+          }
+        }
+      `
+    })
+    .command([
+      "client:codegen",
+      "--target=typescript",
+      "--outputFlat",
+      "API.ts"
+    ])
+    .it("writes types for typescript", () => {
+      expect(fs.readFileSync("API.ts").toString()).toMatchSnapshot();
+    });
 
-// jest.setTimeout(25000);
+  test
+    .fs({
+      "schema.json": JSON.stringify(fullSchema.__schema),
+      "clientSideSchema.graphql": clientSideSchema.toString(),
+      "clientSideSchemaQuery.graphql": clientSideSchemaQuery.toString(),
+      "apollo.config.js": `
+        module.exports = {
+          client: {
+            includes: ["./**.graphql"],
+            service: {
+              name: "my-service-name",
+              localSchemaFile: "./schema.json"
+            }
+          }
+        }
+      `
+    })
+    .command([
+      "client:codegen",
+      "--target=typescript",
+      "--outputFlat",
+      "API.ts"
+    ])
+    .it("writes typescript types for query with client-side data", () => {
+      expect(fs.readFileSync("API.ts").toString()).toMatchSnapshot();
+    });
+});
+
+// OLD TESTS BELOW -- TO REWRITE
 
 // describe("successful codegen", () => {
 // test
