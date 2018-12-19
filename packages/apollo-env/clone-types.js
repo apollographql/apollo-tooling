d052a6ef2a53f214ed426875d66c6f8a7cd1bec0
Jake <dawkinsjh@gmail.com>
2018-12-19 12:19:25 -0500

    rollback copy script to fix hanging windows build

--- packages/apollo-env/clone-types.js
+++ packages/apollo-env/clone-types.js
@@ -6,12 +6,34 @@
  */
 const fs = require("fs");
 const path = require("path");
-const glob = require("glob");
 
-// if the path to the file doesn't exist, we can't write the file
-// this function just recursively creates the dir tree for the file to be written.
+// get list of all files that match filter
+const walk = (dir, filter) => {
+  let fileList = [];
+  const files = fs.readdirSync(dir);
+
+  for (file of files) {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      const subFileList = walk(filePath, filter);
+      fileList = [...fileList, ...subFileList];
+    } else {
+      if (filter && filter.test) {
+        if (filter.test(filePath)) fileList.push(filePath);
+      } else {
+        fileList.push(filePath);
+      }
+    }
+  }
+
+  return fileList;
+};
+
 const makeNestedDir = dir => {
   if (fs.existsSync(dir)) return;
+
   try {
     fs.mkdirSync(dir);
   } catch (err) {
@@ -22,31 +44,19 @@ const makeNestedDir = dir => {
   }
 };
 
-/*
- *  This function copies files ([string]) to the outputDir
- *  and preserves the path in the filename.
- *
- *  e.g. "test/a/b/hello.txt" would create directories "test", "a", and "b"
- */
-copyFilesToDir = (files, outputDir) => {
+copyFilesWithDirStructure = (files, outputDir) => {
   for (file of files) {
-    // replace the src/ directory in the path with the outputDir
-    const pathToWrite = path.join(outputDir, file.replace(/src./, ""));
+    // strip off the src dir
+    const finalPath = path.join(outputDir, file.replace(/src./, ""));
 
     // if it's in a nested dir, we need to create the full path to the file
     // before creating the file
-    makeNestedDir(path.dirname(pathToWrite));
+    makeNestedDir(path.dirname(finalPath));
 
-    fs.copyFileSync(file, pathToWrite);
-    console.log(`copied: ${file} -> ${pathToWrite}`);
+    fs.copyFileSync(file, finalPath);
+    console.log(`copied: ${file} -> ${finalPath}`);
   }
 };
 
-const files = glob
-  .sync("./src/**/*.d.ts", {
-    absolute: true // want path to file, not just filename
-  })
-  // remove the path to the current dir
-  .map(file => file.replace(`${process.cwd()}/`, ""));
-
-copyFilesToDir(files, "lib");
+const files = walk("./src", /.*\.d\.ts/);
+copyFilesWithDirStructure(files, "lib");
