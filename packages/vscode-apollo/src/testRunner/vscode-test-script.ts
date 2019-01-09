72d4aa2bef21cb04c14e1d7859e62763f52e8897
Trevor Scheer <trevor@apollographql.com>
2019-01-09 12:12:42 -0800

    Expand on documentation for test runner

--- packages/vscode-apollo/src/testRunner/vscode-test-script.ts
+++ packages/vscode-apollo/src/testRunner/vscode-test-script.ts
@@ -8,6 +8,10 @@ spawnSync(
     shell: true,
     env: {
       CODE_TESTS_PATH: `${process.cwd()}/lib/testRunner`,
+      // Note:
+      // Extensions that boot up and throw warnings on the host machine will cause tests to fail,
+      // so running with --disable-extensions is a must. This env variable in particular is open
+      // to injection in this manner, so I did it with a sad face.
       CODE_TESTS_WORKSPACE: "--disable-extensions"
     }
   }
