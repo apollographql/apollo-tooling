bcdb90a56ec23f87541a5782dae40c79f5fd1ea8
Trevor Scheer <trevor@apollographql.com>
2019-01-09 12:14:53 -0800

    Fix and cleanup process.stdout/process.stderr nonsense

--- packages/vscode-apollo/src/testRunner/index.ts
+++ packages/vscode-apollo/src/testRunner/index.ts
@@ -7,9 +7,14 @@ import { resolve } from "path";
 import { config } from "./jest-config";
 
 export async function run(_testRoot: string, callback: TestRunnerCallback) {
+  const writeStreamRefs = forwardWriteStreams();
+
   try {
     const testDirectory = resolve(__dirname, "..", "..", "src");
     const { globalConfig, results } = await runCLI(config, [testDirectory]);
+
+    restoreWriteStreams(writeStreamRefs);
+
     const failures = collectTestFailureMessages(results);
 
     if (failures.length > 0) {
@@ -36,6 +41,32 @@ function collectTestFailureMessages(results: ResultsObject): string[] {
   }, []);
 
   return failures;
+}
+
+/**
+ * Forward writes to process.stdout and process.stderr to console.log.
+ *
+ * For some reason this seems to be required for the Jest output to be streamed
+ * to the Debug Console.
+ */
+function forwardWriteStreams() {
+  const outRef = process.stdout.write;
+  const errRef = process.stderr.write;
+
+  process.stdout.write = logger;
+  process.stderr.write = logger;
+
+  return { outRef, errRef };
+}
+
+function restoreWriteStreams(refs: { outRef: any; errRef: any }) {
+  process.stdout.write = refs.outRef;
+  process.stderr.write = refs.errRef;
+}
+
+function logger(line: string) {
+  console.log(line);
+  return true;
 }
 
 export type TestRunnerCallback = (error: Error | null, failures?: any) => void;
--- packages/vscode-apollo/src/testRunner/jest-vscode-environment.ts
+++ packages/vscode-apollo/src/testRunner/jest-vscode-environment.ts
@@ -11,33 +11,14 @@ const NodeEnvironment = require("jest-environment-node");
 import * as vscode from "vscode";
 
 class VsCodeEnvironment extends NodeEnvironment {
-  private stdOutRef: any;
-  private stdErrRef: any;
-
   public async setup() {
     await super.setup();
     this.global.vscode = vscode;
-
-    // Save off refs to the original functions so they can be restored on teardown
-    this.stdOutRef = process.stdout.write;
-    this.stdErrRef = process.stderr.write;
-
-    // This seems to be required for Jest's output to be streamed to the Debug Console
-    process.stdout.write = this.logger;
-    process.stderr.write = this.logger;
   }
 
   public async teardown() {
-    process.stdout.write = this.stdOutRef;
-    process.stderr.write = this.stdErrRef;
-
     this.global.vscode = {};
     return await super.teardown();
-  }
-
-  private logger(line: string) {
-    console.log(line);
-    return true;
   }
 }
 
