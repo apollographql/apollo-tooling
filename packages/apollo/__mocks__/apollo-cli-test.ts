f6b7e08478e3afe12159271e740e1eb9e27176c6
Jake <dawkinsjh@gmail.com>
2019-01-11 14:24:36 -0500

    test without rmdir

--- packages/apollo/__mocks__/apollo-cli-test.ts
+++ packages/apollo/__mocks__/apollo-cli-test.ts
@@ -26,19 +26,19 @@ const debug = fn => {
 };
 
 const deleteFolderRecursive = path => {
-  if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(function(file, index) {
-      var curPath = path + "/" + file;
-      if (fs.lstatSync(curPath).isDirectory()) {
-        // recurse
-        deleteFolderRecursive(curPath);
-      } else {
-        // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
+  // if (fs.existsSync(path)) {
+  //   fs.readdirSync(path).forEach(function(file, index) {
+  //     var curPath = path + "/" + file;
+  //     if (fs.lstatSync(curPath).isDirectory()) {
+  //       // recurse
+  //       deleteFolderRecursive(curPath);
+  //     } else {
+  //       // delete file
+  //       fs.unlinkSync(curPath);
+  //     }
+  //   });
+  //   fs.rmdirSync(path);
+  // }
 };
 
 const makeNestedDir = dir => {
