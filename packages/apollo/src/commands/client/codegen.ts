b14e702420502a129fc90e9284f190ffd7fcf852
Steel Brain <i@steelbrain.me>
2019-01-22 23:25:18 +0500

    :new: Add a useTypescriptGlobalNS flag

--- packages/apollo/src/commands/client/codegen.ts
+++ packages/apollo/src/commands/client/codegen.ts
@@ -91,6 +91,9 @@ export default class Generate extends ClientCommand {
     globalTypesFile: flags.string({
       description:
         'By default, TypeScript will put a file named "globalTypes.ts" inside the "output" directory. Set "globalTypesFile" to specify a different path.'
+    }),
+    useTypescriptGlobalNS: flags.boolean({
+      description: "Use Typescript global namespace"
     })
   };
 
