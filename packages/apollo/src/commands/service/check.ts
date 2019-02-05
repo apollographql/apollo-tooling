0705c74291e1772c353e67f1c8c0955662486b16
Evans Hauser <evanshauser@gmail.com>
2019-02-04 17:01:54 -0800

    apollo: copy types and formatting into check

--- packages/apollo/src/commands/service/check.ts
+++ packages/apollo/src/commands/service/check.ts
@@ -1,10 +1,39 @@
 import { flags } from "@oclif/command";
 import { table } from "heroku-cli-util";
 import { introspectionFromSchema } from "graphql";
+import chalk from "chalk";
 
 import { gitInfo } from "../../git";
-import { ChangeType, format, SchemaChange as Change } from "../../diff";
 import { ProjectCommand } from "../../Command";
+
+// TODO These types can/should be generated from the engine schema when we
+// dogfood codegen inside of the apollo-tooling repo
+interface SchemaChange {
+  type: ChangeType;
+  code: string;
+  description: string;
+}
+enum ChangeType {
+  FAILURE = "FAILURE",
+  WARNING = "WARNING",
+  NOTICE = "NOTICE"
+}
+
+const formatChange = (change: SchemaChange) => {
+  let color = (x: string): string => x;
+  if (change.type === ChangeType.FAILURE) {
+    color = chalk.red;
+  }
+  if (change.type === ChangeType.WARNING) {
+    color = chalk.yellow;
+  }
+
+  return {
+    type: color(change.type),
+    code: color(change.code),
+    description: color(change.description)
+  };
+};
 
 export default class ServiceCheck extends ProjectCommand {
   static aliases = ["schema:check"];
@@ -47,14 +76,14 @@ export default class ServiceCheck extends ProjectCommand {
     const { targetUrl, diffToPrevious } = checkSchemaResult;
     const { changes /*, type, validationConfig */ } = diffToPrevious;
     const failures = changes.filter(
-      ({ type }: Change) => type === ChangeType.FAILURE
+      ({ type }: SchemaChange) => type === ChangeType.FAILURE
     );
 
     if (changes.length === 0) {
       return this.log("\nNo changes present between schemas\n");
     }
     this.log("\n");
-    table(changes.map(format), {
+    table(changes.map(formatChange), {
       columns: [
         { key: "type", label: "Change" },
         { key: "code", label: "Code" },
