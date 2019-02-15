4d32cdbcd717391e8557a053f48c90f7d204557c
Trevor Scheer <trevor@apollographql.com>
2019-02-15 11:52:34 -0800

    Update operation normalization to deterministically sort fragments.
    This includes the work from #1027, #1115 and #1118, which first surfaced in
    an `apollo@next` CLI version which was released in order to provide a
    migration path for paying customers who utilize the Apollo Operation
    Registry through the CLI's `apollo client:push` features.
    
    Those customers were notified and advised to either pin their `apollo`
    version prior to this being released, so the hope is that we'll be able to
    released this under the `apollo@2` cover without incurring breaking changes
    on anyone else.
    
    For more information on the operation registry, see:
    
    https://www.apollographql.com/docs/platform/operation-registry.html
    
    And if you encounter any problems, please contact our customer support via
    Intercom from within your Engine UI.
    
    ---
    
    The summary of the relevant commit messages is below:
    
    1) Remove duplication from client:push and client:extract.
    2) Create a test to verify upcoming changes for this PR.
    
    Cutover to apollo-graphql
    
    * Add apollo-graphql as a dependency (and project reference)
    * Remove apollo-engine-reporting as a dependency
    * Update sortAST to normalize order of fragments w.r.t operations
    * Tests are failing expectedly at this point
    
    Centralize operation hashing function
    
    These two operations will likely be used in tandem, and we want this to be consistent across consumers.
    
    Incorporate rename suggestions
    
    Update version for extracted manifest output.
    
    Use empty string and add comment about unused metadata field.
    
    Revert changes to defaultEngineReportingSignature. Apply changes to new function, defaultOperationRegistrySignature.
    
    This new function is the effective interim fix, and the current existing function is now left alone.
    
    Pass operation name along to the operation registry signature function
    
    Leverage updated registerOperations API
    
    Now that registerOperations supports version as an argument
    to the mutation, we can leverage this for v2 oeprations manifest
    work.

--- packages/apollo-language-server/src/graphqlTypes.ts
+++ packages/apollo-language-server/src/graphqlTypes.ts
@@ -114,6 +114,7 @@ export interface RegisterOperationsVariables {
   id: string;
   clientIdentity: RegisteredClientIdentityInput;
   operations: RegisteredOperationInput[];
+  manifestVersion: number;
 }
 
 /* tslint:disable */
@@ -153,9 +154,6 @@ export interface SchemaTagInfo_service {
 }
 
 export interface SchemaTagInfo {
-  /**
-   * Service by ID
-   */
   service: SchemaTagInfo_service | null;
 }
 
@@ -194,13 +192,7 @@ export interface SchemaTagsAndFieldStats_service_stats_fieldStats_metrics {
 
 export interface SchemaTagsAndFieldStats_service_stats_fieldStats {
   __typename: "ServiceFieldStatsRecord";
-  /**
-   * Dimensions of ServiceFieldStats that can be grouped by.
-   */
   groupBy: SchemaTagsAndFieldStats_service_stats_fieldStats_groupBy;
-  /**
-   * Metrics of ServiceFieldStats that can be aggregated over.
-   */
   metrics: SchemaTagsAndFieldStats_service_stats_fieldStats_metrics;
 }
 
@@ -219,9 +211,6 @@ export interface SchemaTagsAndFieldStats_service {
 }
 
 export interface SchemaTagsAndFieldStats {
-  /**
-   * Service by ID
-   */
   service: SchemaTagsAndFieldStats_service | null;
 }
 
@@ -780,9 +769,6 @@ export interface GetSchemaByTag_service_Service {
 export type GetSchemaByTag_service = GetSchemaByTag_service_User | GetSchemaByTag_service_Service;
 
 export interface GetSchemaByTag {
-  /**
-   * Current identity, null if not authenticated
-   */
   service: GetSchemaByTag_service | null;
 }
 
