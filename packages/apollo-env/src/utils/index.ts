70549359b8046460053084e4eb11934eed85b8b1
Trevor Scheer <trevor@apollographql.com>
2019-02-27 09:31:54 -0800

    Add environment-aware createHash to apollo-env
    Add environment awareness in order to use crypto/sha.js when appropriate

---
+++ packages/apollo-env/src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from "./createHash";
+export * from "./isNode";
---
+++ packages/apollo-env/src/utils/isNode.ts
@@ -0,0 +1,7 @@
+export const isNode =
+  typeof process === "object" &&
+  process &&
+  process.release &&
+  process.release.name === "node" &&
+  process.versions &&
+  typeof process.versions.node === "string";
