3e44d259c7f867b7f0f43b774d2fd829c183126e
Martijn Walraven <martijn@martijnwalraven.com>
2019-03-03 16:35:35 +0100

    Add schema construction to `apollo-graphql`

---
+++ packages/apollo-graphql/src/types/graphql.ts
@@ -0,0 +1,32 @@
+import { DocumentNode, GraphQLSchema, GraphQLError } from "graphql";
+
+import { SDLValidationRule } from "graphql/validation/ValidationContext";
+
+// FIXME: These typings should all be added to `@types/graphql`.
+
+declare module "graphql/validation/validate" {
+  function validateSDL(
+    documentAST: DocumentNode,
+    schemaToExtend?: GraphQLSchema | null,
+    rules?: ReadonlyArray<SDLValidationRule>
+  ): GraphQLError[];
+}
+
+declare module "graphql/type/schema" {
+  interface GraphQLSchema {
+    toConfig(): GraphQLSchemaConfig;
+  }
+}
+
+declare module "graphql/type/definition" {
+  interface GraphQLObjectType {
+    toConfig(): GraphQLObjectTypeConfig<any, any> & {
+      interfaces: GraphQLInterfaceType[];
+      fields: GraphQLFieldConfigMap<any, any>;
+    };
+  }
+
+  interface GraphQLField<TSource, TContext, TArgs = { [key: string]: any }> {
+    toConfig(): GraphQLFieldConfig<TSource, TContext, TArgs>;
+  }
+}
