fcef15d734205b8e42b168c7f94e1551776d0efd
Martijn Walraven <martijn@martijnwalraven.com>
2019-03-03 16:35:35 +0100

    Add schema construction to `apollo-graphql`

--- packages/apollo-graphql/src/schema/GraphQLSchemaValidationError.ts
+++ packages/apollo-graphql/src/schema/GraphQLSchemaValidationError.ts
@@ -1,4 +1,4 @@
-import { GraphQLError } from "graphql";
+import { GraphQLError, printError } from "graphql";
 
 export class GraphQLSchemaValidationError extends Error {
   constructor(public errors: ReadonlyArray<GraphQLError>) {
@@ -7,5 +7,9 @@ export class GraphQLSchemaValidationError extends Error {
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
     this.message = errors.map(error => error.message).join("\n\n");
+  }
+
+  toString() {
+    return this.errors.map(error => printError(error)).join("\n\n");
   }
 }
