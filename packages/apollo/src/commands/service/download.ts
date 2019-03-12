75cec7541a813f56ab8921ba895a260e266a93c4
Jake <dawkinsjh@gmail.com>
2019-03-12 14:58:39 -0400

    Add ability to download schema for clients

--- packages/apollo/src/commands/service/download.ts
+++ packages/apollo/src/commands/service/download.ts
@@ -1,12 +1,12 @@
 import { flags } from "@oclif/command";
 import { introspectionFromSchema } from "graphql";
 import { writeFileSync } from "fs";
-
 import { ProjectCommand } from "../../Command";
 
 export default class ServiceDownload extends ProjectCommand {
   static aliases = ["schema:download"];
-  static description = "Download the schema from your GraphQL endpoint.";
+  static description =
+    "Download a schema from a locally running GraphQL endpoint.";
 
   static flags = {
     ...ProjectCommand.flags,
@@ -37,11 +37,21 @@ export default class ServiceDownload extends ProjectCommand {
       {
         title: `Saving schema to ${args.output}`,
         task: async () => {
-          const schema = await project.resolveSchema({ tag: flags.tag });
-          writeFileSync(
-            args.output,
-            JSON.stringify(introspectionFromSchema(schema), null, 2)
-          );
+          try {
+            const schema = await project.resolveSchema({ tag: flags.tag });
+            writeFileSync(
+              args.output,
+              JSON.stringify(introspectionFromSchema(schema), null, 2)
+            );
+          } catch (e) {
+            if (e.code == "ECONNREFUSED") {
+              console.error(
+                `❌ ERROR: Connection refused. You may not be running a service locally, or your endpoint url is incorrect.\n` +
+                  "If you're trying to download a schema from Apollo Engine, use the `client:download schema` command instead."
+              );
+            }
+            throw e;
+          }
         }
       }
     ]);
