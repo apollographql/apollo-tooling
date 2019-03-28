47bbcc0afd7437aa110180158e8eced06465c3eb
Adrien HARNAY <adrien@harnay.me>
2019-03-28 17:21:03 +0100

    add flag description

--- packages/apollo/src/commands/client/codegen.ts
+++ packages/apollo/src/commands/client/codegen.ts
@@ -91,6 +91,10 @@ export default class Generate extends ClientCommand {
     globalTypesFile: flags.string({
       description:
         'By default, TypeScript will put a file named "globalTypes.ts" inside the "output" directory. Set "globalTypesFile" to specify a different path. Alternatively, set "fileExtension" to modify the extension of the file'
+    }),
+    fileExtension: flags.string({
+      description:
+        'By default, TypeScript will output ".ts" files. Set "fileExtension" to specify a different file extension'
     })
   };
 
