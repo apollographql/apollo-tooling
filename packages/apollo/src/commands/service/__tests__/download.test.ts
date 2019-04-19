c080e0fea90dd21af605e7ab7436c0155aadd94c
Jake <dawkinsjh@gmail.com>
2019-04-19 13:00:18 -0700

    enabled service:download test suite

--- packages/apollo/src/commands/service/__tests__/download.test.ts
+++ packages/apollo/src/commands/service/__tests__/download.test.ts
@@ -1,74 +1,68 @@
-it("is turned on after summit", () => {});
+// this is because of herkou-cli-utils hacky mocking system on their console logger
+import { stdout, mockConsole } from "heroku-cli-util";
+import path from "path";
+import fs from "fs";
+import { test as setup } from "apollo-cli-test";
+import {
+  introspectionQuery,
+  print,
+  execute,
+  buildSchema,
+  graphql
+} from "graphql";
+import gql from "graphql-tag";
 
-// jest.mock("apollo-codegen-core/lib/localfs", () => {
-//   return require("../../../__mocks__/localfs");
-// });
+const test = setup.do(() => mockConsole());
+const fullSchema = execute(
+  buildSchema(
+    fs.readFileSync(path.resolve(__dirname, "./fixtures/schema.graphql"), {
+      encoding: "utf-8"
+    })
+  ),
+  gql(introspectionQuery)
+).data;
 
-// // this is because of herkou-cli-utils hacky mocking system on their console logger
-// import { stdout, mockConsole } from "heroku-cli-util";
-// import path from "path";
-// import fs from "fs";
-// import { test as setup } from "apollo-cli-test";
-// import { introspectionQuery, print, execute, buildSchema } from "graphql";
-// import gql from "graphql-tag";
-// import { fs as mockFS, vol } from "apollo-codegen-core/lib/localfs";
+const localSuccess = nock => {
+  nock
+    .post("/graphql", {
+      query: print(gql(introspectionQuery)),
+      operationName: "IntrospectionQuery",
+      variables: {}
+    })
+    .reply(200, { data: fullSchema });
+};
 
-// const test = setup.do(() => mockConsole());
-// const fullSchema = execute(
-//   buildSchema(
-//     fs.readFileSync(path.resolve(__dirname, "./fixtures/schema.graphql"), {
-//       encoding: "utf-8"
-//     })
-//   ),
-//   gql(introspectionQuery)
-// ).data;
+jest.setTimeout(25000);
 
-// const localSuccess = nock => {
-//   nock
-//     .post("/graphql", {
-//       query: print(gql(introspectionQuery)),
-//       operationName: "IntrospectionQuery",
-//       variables: {}
-//     })
-//     .reply(200, { data: fullSchema });
-// };
+describe("successful schema downloading", () => {
+  // right now, unless a config flag is set, it will search parent dirs
+  // for configs, finding the config file for this repo (which we don't want)
+  // to avoid that, I added an empty config and a config flag pointing to that.
+  test
+    .fs({ "my.config.js": "module.exports= {}" })
+    .nock("http://localhost:4000", localSuccess)
+    .command([
+      "service:download",
+      "--endpoint=http://localhost:4000/graphql",
+      "--config=my.config.js"
+    ])
+    .it("grabs schema JSON from local server", () => {
+      expect(fs.readFileSync("schema.json").toString()).toMatchSnapshot();
+    });
 
-// beforeEach(() => {
-//   vol.reset();
-//   vol.fromJSON({
-//     __blankFileSoDirectoryExists: ""
-//   });
-// });
-
-// jest.setTimeout(25000);
-
-// describe("successful schema downloading", () => {
-//   test
-//     .nock("http://localhost:4000", localSuccess)
-//     .command(["service:download", "--endpoint=http://localhost:4000/graphql"])
-//     .it("grabs schema JSON from local server", () => {
-//       expect(mockFS.readFileSync("schema.json").toString()).toMatchSnapshot();
-//     });
-
-//   test
-//     .do(() =>
-//       vol.fromJSON({
-//         "package.json": `
-//       {
-//         "apollo": {
-//           "schemas": {
-//             "localServer": {
-//               "endpoint": "http://localhost:1234/graphql"
-//             }
-//           }
-//         }
-//       }
-//       `
-//       })
-//     )
-//     .nock("http://localhost:1234", localSuccess)
-//     .command(["service:download"])
-//     .it("grabs schema JSON from local server specified in config", () => {
-//       expect(mockFS.readFileSync("schema.json").toString()).toMatchSnapshot();
-//     });
-// });
+  test
+    .fs({
+      "apollo.config.js": `
+        module.exports = {
+          "service": {
+            "endpoint": { "url": "http://localhost:1234/graphql" },
+          }
+        }
+      `
+    })
+    .nock("http://localhost:1234", localSuccess)
+    .command(["service:download"])
+    .it("grabs schema JSON from local server specified in config", () => {
+      expect(fs.readFileSync("schema.json").toString()).toMatchSnapshot();
+    });
+});
