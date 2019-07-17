b0515b3e4854243ad5d99c12e0a5b008e94b70b5
Jesse Rosenberger <git@jro.cc>
2019-07-17 20:03:30 +0300

    Meh.

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import json from "rollup-plugin-json";
+import nodeResolve from "rollup-plugin-node-resolve";
 import typescriptPlugin from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
 import builtins from "rollup-plugin-node-builtins";
@@ -16,40 +18,48 @@ export function rollup({
   const projectDir = path.join(__filename, "..");
   console.info(`Building project esm ${projectDir}`);
   const tsconfig = `${projectDir}/tsconfig.json`;
+  const external = Object.keys(
+    require(`${projectDir}/package.json`).dependencies
+  );
 
   function outputFile(format) {
     return "./lib/" + outputPrefix + "." + format + ".js";
   }
 
-  const format = "umd";
+  const format = "iife";
   return [
     {
+      external,
       input,
       output: {
-        name: "apollo-language-server",
+        name: "apolloLanguageServer",
         file: outputFile(format),
         format,
         sourcemap: true
       },
       plugins: [
-        // globals(),
-        // nodeResolve({
-        //   extensions: [".ts", ".tsx"],
-        //   mainFields: ["main", "module"]
+        globals(),
+
+        // This can't be used right now because we're using `existsSync`, which
+        // is not included in the browserify-fs stub provider in level-filesystem.
+        // Realistically, that's likely a problem, so that should probably be
+        // switched, or that code-path avoided.
+        //
+        // builtins({
+        //   fs: true,
         // }),
+
+        nodeResolve({
+          extensions: [".ts", ".tsx", ".js"]
+          // mainFields: ["browser", "main"]
+        }),
+
         typescriptPlugin({ typescript, tsconfig }),
+
         commonjs({
           include: /node_modules/
         })
-        // builtins(),
-      ],
-      onwarn(message) {
-        const suppressed = ["UNRESOLVED_IMPORT", "THIS_IS_UNDEFINED"];
-
-        if (!suppressed.find(code => message.code === code)) {
-          return console.warn(message.message);
-        }
-      }
+      ]
     }
   ];
 }
