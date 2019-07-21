2a6b410a0475c861f9209516f693828e9ea7d4ff
Jake <dawkinsjh@gmail.com>
2019-07-21 19:56:06 -0400

    Update debug to warn properly with CLI

--- packages/apollo-language-server/src/utilities/debug.ts
+++ packages/apollo-language-server/src/utilities/debug.ts
@@ -1,37 +1,44 @@
 import { IConnection } from "vscode-languageserver";
 
 export class Debug {
-  public static connection: IConnection;
+  public static connection?: IConnection;
 
   public static SetConnection(conn: IConnection) {
     Debug.connection = conn;
   }
 
   public static info(message: string) {
-    Debug.connection.sendNotification("serverDebugMessage", {
-      type: "info",
-      message: message
-    });
+    Debug.connection
+      ? Debug.connection.sendNotification("serverDebugMessage", {
+          type: "info",
+          message: message
+        })
+      : console.log("[INFO] " + message);
   }
 
   public static error(message: string) {
-    Debug.connection.sendNotification("serverDebugMessage", {
-      type: "error",
-      message: message
-    });
+    Debug.connection
+      ? Debug.connection.sendNotification("serverDebugMessage", {
+          type: "error",
+          message: message
+        })
+      : console.error("[ERROR] " + message);
   }
 
   public static warning(message: string) {
-    Debug.connection.sendNotification("serverDebugMessage", {
-      type: "warning",
-      message: message
-    });
+    Debug.connection
+      ? Debug.connection.sendNotification("serverDebugMessage", {
+          type: "warning",
+          message: message
+        })
+      : console.warn("[WARNING] " + message);
   }
 
   public static sendErrorTelemetry(message: string) {
-    Debug.connection.sendNotification("serverDebugMessage", {
-      type: "errorTelemetry",
-      message: message
-    });
+    Debug.connection &&
+      Debug.connection.sendNotification("serverDebugMessage", {
+        type: "errorTelemetry",
+        message: message
+      });
   }
 }
