5bf29a8714a9e7b9dc193bd598fc5de7b8832db9
Ellen Shapiro <designatednerd@gmail.com>
2019-09-24 15:17:14 -0500

    Period at end of comment

--- packages/apollo-codegen-swift/src/codeGeneration.ts
+++ packages/apollo-codegen-swift/src/codeGeneration.ts
@@ -223,7 +223,7 @@ export class SwiftAPIGenerator extends SwiftGenerator<CompilerContext> {
       },
       () => {
         if (source) {
-          this.comment("The raw GraphQL definition of this operation");
+          this.comment("The raw GraphQL definition of this operation.");
           this.printOnNewline(swift`public let operationDefinition =`);
           this.withIndent(() => {
             this.multilineString(source, suppressMultilineStringLiterals);
@@ -342,7 +342,7 @@ export class SwiftAPIGenerator extends SwiftGenerator<CompilerContext> {
       outputIndividualFiles,
       () => {
         if (source) {
-          this.comment("The raw GraphQL definition of this fragment");
+          this.comment("The raw GraphQL definition of this fragment.");
           this.printOnNewline(swift`public static let fragmentDefinition =`);
           this.withIndent(() => {
             this.multilineString(source, suppressMultilineStringLiterals);
