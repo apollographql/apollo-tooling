60c8d48f204c7727dbfd035fe99fda797b9da81a
Trevor Scheer <trevor@apollographql.com>
2019-10-22 09:38:40 -0700

    Remove snapshotSerializers since it's extraneous

--- packages/apollo-tools/jest.config.js
+++ packages/apollo-tools/jest.config.js
@@ -2,9 +2,5 @@ const baseConfig = require("../../jest.base");
 
 module.exports = {
   ...baseConfig,
-  setupFiles: ["apollo-env"],
-  testPathIgnorePatterns: [
-    ...baseConfig.testPathIgnorePatterns,
-    "snapshotSerializers"
-  ]
+  setupFiles: ["apollo-env"]
 };
