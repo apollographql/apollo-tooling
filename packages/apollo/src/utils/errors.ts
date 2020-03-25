fdb552f9dbec898617a83726162a35b96060b4b0
Adam Zionts <adam@meteor.com>
2020-03-24 20:07:37 -0700

    Make newlines (optional commit)

--- packages/apollo/src/utils/errors.ts
+++ packages/apollo/src/utils/errors.ts
@@ -5,5 +5,5 @@ const errorMessage = [
   "In order to run this command, please provide a graph ID using the 'apollo.config.js' file.",
   "\n\nFor more information on configuring the Apollo CLI, please go to",
   "https://go.apollo.dev/t/config"
-].join(" ");
+].join("\n");
 export const graphUndefinedError = new CLIError(errorMessage);
