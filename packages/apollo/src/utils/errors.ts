aed6a1da19a0a4d2d9fa075055169c73ecf6d8c2
Adam Zionts <adam@apollographql.com>
2020-03-25 10:54:24 -0700

    Update packages/apollo/src/utils/errors.ts
    Co-Authored-By: Jake Dawkins <dawkinsjh@gmail.com>

--- packages/apollo/src/utils/errors.ts
+++ packages/apollo/src/utils/errors.ts
@@ -4,6 +4,6 @@ const errorMessage = [
   "No graph (i.e. service) found to link to Apollo Graph Manager.",
   "In order to run this command, please provide a graph ID using the 'apollo.config.js' file.",
   "\n\nFor more information on configuring the Apollo CLI, please go to",
-  "https://www.apollographql.com/docs/devtools/apollo-config/"
+  "https://go.apollo.dev/t/config"
 ].join(" ");
 export const graphUndefinedError = new CLIError(errorMessage);
