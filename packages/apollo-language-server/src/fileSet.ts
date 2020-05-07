56cdb033466dd43bdb2c907e2f143a0aa27e1c50
Mathias Vestergaard <mv@nisportal.com>
2020-05-07 11:42:15 +0200

    Add file system watching to FileSet using chokidar for detecting changes to watched files
    Only perform the glob on the initial call to allFiles
    
    Fixes #1061

--- packages/apollo-language-server/src/fileSet.ts
+++ packages/apollo-language-server/src/fileSet.ts
@@ -1,14 +1,17 @@
-import { relative } from "path";
-import minimatch = require("minimatch");
 import glob from "glob";
 import { invariant } from "@apollographql/apollo-tools";
 import URI from "vscode-uri";
 import { normalizeURI } from "./utilities";
+import chokidar from "chokidar";
+import path from "path";
 
 export class FileSet {
   private rootURI: URI;
   private includes: string[];
   private excludes: string[];
+  private files: string[] = [];
+  private watcher: chokidar.FSWatcher;
+  private ready: boolean = false;
 
   constructor({
     rootURI,
@@ -28,6 +31,25 @@ export class FileSet {
     this.rootURI = rootURI;
     this.includes = includes;
     this.excludes = excludes;
+
+    this.watcher = chokidar.watch(includes, {
+      cwd: rootURI.fsPath,
+      ignored: excludes,
+      ignoreInitial: true
+    });
+
+    const absRoot = path.resolve(rootURI.fsPath);
+    function getFullPath(p: string) {
+      return normalizeURI(path.join(absRoot, p));
+    }
+
+    this.watcher.on("add", p => this.files.push(getFullPath(p)));
+    this.watcher.on("unlink", p => {
+      const index = this.files.indexOf(getFullPath(p));
+      if (index > -1) {
+        this.files.splice(index, 1);
+      }
+    });
   }
 
   includesFile(filePath: string): boolean {
@@ -35,11 +57,20 @@ export class FileSet {
   }
 
   allFiles(): string[] {
+    if (!this.ready) {
+      this.globInitial();
+      this.ready = true;
+    }
+
+    return this.files;
+  }
+
+  private globInitial() {
     // since glob.sync takes a single pattern, but we allow an array of `includes`, we can join all the
     // `includes` globs into a single pattern and pass to glob.sync. The `ignore` option does, however, allow
     // an array of globs to ignore, so we can pass it in directly
     const joinedIncludes = `{${this.includes.join(",")}}`;
-    return glob
+    this.files = glob
       .sync(joinedIncludes, {
         cwd: this.rootURI.fsPath,
         absolute: true,
